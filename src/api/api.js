a80c1a935324dd92f121bff9cc5d5c57b0904c99
Jonathan Aldas <jonaaldas@gmail.com>
2022-06-02 10:21:26 -0500

    added new stuff

--- src/api/api.js
+++ src/api/api.js
@@ -1,83 +1,38 @@
 import axios from 'axios'
 
 export const getTransactionsRequest = async () => {
-  return await axios.get('https://transactions-managment.herokuapp.com/transactions', {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.get('https://transactions-managment.herokuapp.com/transactions' )
 }
 
 export const createTransactionsRequest = async (post) => {
-  return await axios.post('https://transactions-managment.herokuapp.com/transactions/add/', post,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.post('https://transactions-managment.herokuapp.com/transactions/add/', post)
 }
 
 export const archiveTransactionRequest = async (id) => {
-  return await axios.delete('https://transactions-managment.herokuapp.com/transactions/' + id,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.delete('https://transactions-managment.herokuapp.com/transactions/' + id)
 }
 
 export const deleteTransactionRequest = async (id) => {
-  return await axios.delete('https://transactions-managment.herokuapp.com/transactions/delete/' + id,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.delete('https://transactions-managment.herokuapp.com/transactions/delete/' + id)
 }
 
 export const getSingleTransactionToEditRequest = async (id) => {
-  return await axios.get('https://transactions-managment.herokuapp.com/transactions/edit/' + id,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.get('https://transactions-managment.herokuapp.com/transactions/edit/' + id)
 }
 
 export const getSingleTransactionToViewRequest = async (id) => {
-  return await axios.get('https://transactions-managment.herokuapp.com/transactions/' + id,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.get('https://transactions-managment.herokuapp.com/transactions/' + id)
 }
 
 export const editTransactionRequest = async (id, newFields) => {
-  return await axios.put('https://transactions-managment.herokuapp.com/transactions/' + id, newFields,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.put('https://transactions-managment.herokuapp.com/transactions/' + id, newFields)
 }
 
 
 export const getAllArchivedTransactionsRequest = async () => {
-  return await axios.get('https://transactions-managment.herokuapp.com/transactions/archived',{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.get('https://transactions-managment.herokuapp.com/transactions/archived')
 }
 
 export const restoreASingleTransactionRequest = async (id) => {
-  return await axios.get('https://transactions-managment.herokuapp.com/transactions/restore/archived/' + id,{
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    }
-  })
+  return await axios.get('https://transactions-managment.herokuapp.com/transactions/restore/archived/' + id)
 }
